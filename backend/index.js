1d8f4a8319071b0c6f4e20c927310b0d0f268639
VidhanThakur09 <vidhanthakur2700@gmail.com>
2025-08-14 23:00:12 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import Response_genrator from './Response_genrator.js';
+import cors from 'cors';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+
+app.post('/hiteshsir', async (req, res) => {
+    const {question,history} = req.body;
+  const response = await Response_genrator("Hitesh Choudhary", question, history);
+  res.send(response);
+});
+
+app.post('/piyushsir', async (req, res) => {
+    const {question,history} = req.body;
+  const response = await Response_genrator("Piyush Garg", question, history);
+  res.send(response);
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
